8bdc626ea1087c55bb3036a5ef59b60e6cd070c0
Maxwell Beck <max@ryt.one>
2020-10-25 22:11:03 -0500

    Derive font size from slide root size

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import {render} from "solid-js/dom";
 import Slides from "./user/slides";
 
-render(() => <Slides />, document.getElementById("root")!);
+const root = document.getElementById("root")!;
+window.addEventListener("resize", (_: UIEvent) => {
+    root.style.setProperty("font-size", (root.clientHeight * 32 / 1080).toString() + "px");
+});
+root.style.setProperty("font-size", (root.clientHeight * 32 / 1080).toString() + "px");
+
+render(() => <Slides />, root);
