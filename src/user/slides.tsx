363e9cb9fe3808e1c37c4a796aac12a188b0d061
Maxwell Beck <max@ryt.one>
2020-11-13 14:20:12 -0600

    Update example slides.tsx to final used for Autumn Jam III.

--- src/user/slides.tsx
+++ src/user/slides.tsx
@@ -5,9 +5,8 @@ export default function Slides(props: {compos: Compo[]}) {
     // TODO is there a way we can get things to work without wrapping slides in closures?
     return <Switcher>
         {() => <CompoSlide uri="assets/TheBuildup/TBLOOP.mp4" compo={props.compos[0]} />}
-        {() => <IntermissionSlide uri="assets/Intermission/INTSTILL.png" text="BE RIGHT BACK!" length={15 * 60} />}
         {() => <CompoSlide uri="assets/TheChase/TCLOOP.mp4" compo={props.compos[1]} />}
-        {() => <IntermissionSlide uri="assets/Intermission/INTSTILL.png" text="BE RIGHT BACK!" length={15 * 60} />}
+        {() => <IntermissionSlide uri="assets/Intermission/INTSTILL.png" text="BE RIGHT BACK!" length={10 * 60} />}
         {() => <CompoSlide uri="assets/SpookyCovers/CVLOOP.mp4" compo={props.compos[2]} />}
     </Switcher>;
 }
