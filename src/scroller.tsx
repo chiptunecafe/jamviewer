3e700fc9b1f51ad064d800bbccf6030482153ced
Maxwell Beck <max@ryt.one>
2020-11-13 14:19:41 -0600

    Fix bug with consecutive scrolling entries

--- src/scroller.tsx
+++ src/scroller.tsx
@@ -4,15 +4,16 @@ export default function Scroller(props: {text: string, ty: string}) {
     let el: HTMLParagraphElement | undefined = undefined;
     let scrolling = false;
     let scrollpos = 0;
+    let scrollwidth = 0;
 
-    const scroll_routine = (width: number) => () => {
+    const scroll_routine = () => {
         if (scrolling) {
-            if ((scrollpos += 1.5) > width) {
+            if ((scrollpos += 1.5) > scrollwidth) {
                 scrollpos = 0;
             }
             el!.scrollLeft = scrollpos;
 
-            window.requestAnimationFrame(scroll_routine(width));
+            window.requestAnimationFrame(scroll_routine);
         } else {
             scrollpos = 0;
             el!.scrollLeft = 0;
@@ -30,12 +31,12 @@ export default function Scroller(props: {text: string, ty: string}) {
         scrolling = display_width < total_width;
         if (scrolling) {
             el!.innerHTML += "&nbsp;~&nbsp;";
-            const w = el!.scrollWidth;
+            scrollwidth = el!.scrollWidth;
             el!.innerHTML += el!.innerHTML;
             el!.style.setProperty("mask-image", "linear-gradient(to right, rgba(0,0,0,0), rgba(0,0,0,1) 10%, rgba(0,0,0,1) 90%, rgba(0,0,0,0))");
 
             if (!already_scrolling) {
-                window.requestAnimationFrame(scroll_routine(w));
+                window.requestAnimationFrame(scroll_routine);
             } else {
                 scrollpos = 0;
             }
